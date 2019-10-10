570c998fa76534660746d07a1a9d381989d887d1
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-10 13:21:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,7 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-    if array[index] = 
+    if array[index] == value_to_find
+      break
 end
 
 def find_max_value(array)
